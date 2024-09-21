a588b1bc8e3ca094125b8c35e4702c49cd84a619
zadrach caunca <zad@seabudigital.com>
2024-09-21 09:15:43 +0800

    Refactor unused imports in chart component

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -1,5 +1,7 @@
 "use client";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
 
 import * as React from "react";
 import * as RechartsPrimitive from "recharts";
